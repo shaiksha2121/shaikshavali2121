849ece3956a723325d885196929fcae5784e1d4c
shaik2121vali@gmail.com <shaik2121vali@gmail.com>
2024-06-03 06:06:20 +0000

    commit

---
+++ models/Trg_Prod_Inc.sql
@@ -0,0 +1,8 @@
+{{config(materialized='incremental',
+          unique_key='PROD_ID')}}
+
+select * from Prod.src_product
+{%if is_incremental()%}
+where 
+UPDATED_DT>(select max(UPDATED_DT) from{{this}})
+{%endif%}
---
+++ models/test1.sql
@@ -0,0 +1,4 @@
+with cte1 as(
+select * from Prod.SRC_Product)
+
+select * from cte1
