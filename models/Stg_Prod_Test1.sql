0493bc8e16eb667adff8503986e128fb116b34e3
shaik2121vali@gmail.com <shaik2121vali@gmail.com>
2024-06-10 07:30:17 +0000

    commit

--- models/Stg_Prod_Test1.sql
+++ models/Stg_Prod_Test1.sql
@@ -1,7 +1,7 @@
 with Product as(
-select PRODUCT_NAME,state,REGION,unit_sold,sum(amount) from 
-DIM_PRODUCT A inner join 
-FACT_SALES B on A.PRODUCT_ID=B.PRODUCT_ID
+select A.PRODUCT_NAME,A.state,A.REGION,B.unit_sold,sum(amount) from 
+Prod.DIM_PRODUCT A inner join 
+Prod.FACT_SALES B on A.PRODUCT_ID=B.PRODUCT_ID
 group by 
-1,2,3,4)
+A.PRODUCT_NAME,A.state,A.REGION,B.unit_sold)
 select * from Product
---
+++ tests/Amount_Sold_Test1.sql
@@ -0,0 +1,2 @@
+select *from {{ref("Stg_Prod_Test1")}}
+where unit_sold<0
